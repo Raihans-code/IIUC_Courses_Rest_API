8875f9af796a2b3d430584d18378b1257b1be9b9
Raihan Islam <iraihan705@gmail.com>
2025-11-16 20:01:01 +0600

    Initial Commit

---
+++ iiuc-course-api/src/main/java/com/iiuc/cse/b58/s3f/g5/iiuc_course_api/IiucCourseApiApplication.java
@@ -0,0 +1,13 @@
+package com.iiuc.cse.b58.s3f.g5.iiuc_course_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IiucCourseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IiucCourseApiApplication.class, args);
+	}
+
+}
---
+++ iiuc-course-api/src/test/java/com/iiuc/cse/b58/s3f/g5/iiuc_course_api/IiucCourseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iiuc.cse.b58.s3f.g5.iiuc_course_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IiucCourseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
